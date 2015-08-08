c57787e363539c8d8449e11d6b1f47b962445058
acpardal <andrecspardal@gmail.com>
2015-08-08 20:52:48 +0100

    Testing automatic config

---
+++ src/soundsystem/CDPlayer.java
@@ -0,0 +1,27 @@
+package soundsystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//@Named
+@Component
+public class CDPlayer implements MediaPlayer {
+	private CompactDisc cd;
+
+	//@Inject
+	@Autowired(required=false)
+	public CDPlayer(CompactDisc cd) {
+		this.cd = cd;
+	}
+
+	@Override
+	public void play() {
+		cd.play();
+	}
+	
+	//@Inject
+	@Autowired
+	public void setCompactDisc(CompactDisc cd) {
+		this.cd = cd;
+	}
+}
--- src/soundsystemTests/CDPlayerTest.java
+++ src/soundsystemTests/CDPlayerTest.java
@@ -1,8 +1,10 @@
 package soundsystemTests;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -10,16 +12,32 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import soundsystem.CDPlayerConfig;
 import soundsystem.CompactDisc;
+import soundsystem.MediaPlayer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=CDPlayerConfig.class)
 public class CDPlayerTest {
 
+	@Rule
+	public final SystemOutRule log = new SystemOutRule();
+	
+	@Autowired
+	private MediaPlayer player;
+	
 	@Autowired
 	private CompactDisc cd;
 	
 	@Test
 	public void cdShouldNotBeNull() {
 		assertNotNull(cd);
+	}
+	
+	@Test
+	public void play() {
+		player.play();
+		assertEquals(
+				"Playing Sgt. Pepper's Lonely Hearts Club Band" +
+				" by The Beatles\n",
+				log.getLog());
 	}
 }
