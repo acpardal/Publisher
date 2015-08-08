cfde089562a9e33fbc408d269f7feff1f5bfd319
acpardal <andrecspardal@gmail.com>
2015-08-08 21:42:13 +0100

    New compactDisc Impl and tested bean creation with java configuration instead of Component Scanning

--- src/soundsystem/CDPlayerConfig.java
+++ src/soundsystem/CDPlayerConfig.java
@@ -1,10 +1,19 @@
 package soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages="soundsystem"/*basePackageClasses={CompactDisc.class}*/)
+//@ComponentScan(basePackages="soundsystem"/*basePackageClasses={CompactDisc.class}*/)
 public class CDPlayerConfig {
 
+	@Bean
+	public CompactDisc sgtPeppers() {
+		return new SgtPeppers();
+	}
+	
+	@Bean
+	public MediaPlayer cdPlayer(CompactDisc cd) {
+		return new CDPlayer(cd);
+	}
 }
